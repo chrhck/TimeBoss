d12c19543bb5d001007712d7cc3e495a202c1f69
Christian Haack <43646111+chrhck@users.noreply.github.com>
2020-10-28 23:01:55 +0100

    Update setup.py
    advance version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TimeBoss", # Replace with your own username
-    version="0.1.2",
+    version="0.1.3",
     author="Christian Haack",
     author_email="chr.hck@gmail.com",
     description="A lightweight python timing class",
